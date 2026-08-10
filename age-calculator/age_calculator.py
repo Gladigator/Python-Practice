d5b5e551fbd392cbffdba3c206499ff54fdf7712
Gladigator <43288224+Gladigator@users.noreply.github.com>
2026-08-10 20:34:00 +1000

    Add age calculator scriptAdd age calculator project
    This script calculates the user's age based on their birth year and the current year, and also determines the year they will turn 100.

---
+++ age-calculator/age_calculator.py
@@ -0,0 +1,23 @@
+# === Enter Your Birthday ===
+name = " Alice"
+birth_year = 1995
+print("Name:" + name)
+print("Birth year:" + " " +  str(birth_year))
+
+# === Calculate Your Age ===
+current_year = 2024
+age = current_year - birth_year
+print("Current year:" + " " +  str(current_year))
+print("Age:" + " " + str(age))
+
+# === Your 100th Birthday ===
+name = " Alice"
+birth_year = 1995
+print("Name:" + name)
+print("Birth year:" + " " +  str(birth_year))
+current_year = 2024
+age = current_year - birth_year
+print("Current year:" + " " +  str(current_year))
+print("Age:" + " " + str(age))
+century_year = birth_year + 100
+print("You will turn 100 in:" + " " + str(century_year))
